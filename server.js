9bdf254c2a2b302b0c0f2491b600b607e31fe9cb
JAYDEN2904 <osafojayden290405@gmail.com>
2023-12-18 09:19:54 +0000

    second functionality

--- server.js
+++ server.js
@@ -21,6 +21,17 @@ app.get('/patients', async(req, res) => {
   }
 })
 
+app.get('/patients/:id', async(req, res) => {
+  try {
+    const {id} = req.params
+    const patient = await Patient.findById(id);
+    res.status(200).json(patient);
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+
 
 
 mongoose.
